7254393e88cc80fd4947fe4ef06718990ffdeaad
RayhanArrazy123 <125890079+RayhanArrazy123@users.noreply.github.com>
2023-02-20 22:40:15 +0700

    Add files via upload

---
+++ portfolio2.php
@@ -0,0 +1,83 @@
+<div id="ajax-page" class="ajax-page-content">
+    <div class="ajax-page-wrapper">
+        <div class="ajax-page-nav">
+            <div class="nav-item ajax-page-prev-next">
+                <a class="ajax-page-load" href="portfolio_1.php"><i class="lnr lnr-chevron-left"></i></a>
+            </div>
+            <div class="nav-item ajax-page-close-button">
+                <a id="ajax-page-close-button" href="#"><i class="lnr lnr-cross"></i></a>
+            </div>
+        </div>
+
+        <div class="ajax-page-title">
+            <h1>Portfolio Project 2</h1>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-8 col-md-8 portfolio-block">
+                <div class="owl-carousel portfolio-page-carousel">
+                    <div class="item">
+                        <img src="img/portfolio/couro1.png" alt="" />
+                    </div>
+                    <div class="item">
+                        <img src="img/portfolio/couro2.png" alt="" />
+                    </div>
+                    <div class="item">
+                        <img src="img/portfolio/couro3.png" alt="" />
+                    </div>
+                </div>
+
+                <div class="portfolio-page-video embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="img/RecordCouro.mp4"></iframe>
+                </div>
+
+                <script type="text/javascript">
+                    jQuery(document).ready(function($){
+                        $('.portfolio-page-carousel').imagesLoaded(function(){
+                            $('.portfolio-page-carousel').owlCarousel({
+                                smartSpeed:1200,
+                                items: 1,
+                                loop: true,
+                                dots: true,
+                                nav: true,
+                                navText: false,
+                                margin: 10,
+                                autoHeight:true
+                            });
+                        });
+                    });
+                </script>
+            </div>
+
+            <div class="col-sm-4 col-md-4 portfolio-block">
+                <!-- Project Description -->
+                <div class="project-description">
+                    <div class="block-title">
+                        <h3>Description</h3>
+                    </div>
+                    <ul class="project-general-info">
+                        <li><p><i class="fa fa-user"></i> Project Akhir Kelas 10</p></li>
+                        <li><p><i class="fa fa-globe"></i> <a href="https://couro.budiluhur.my.id/" target="_blank">Link Website E-commerce</p></li>
+                        <li><p><i class="fa fa-calendar"></i> April 2021</p></li>
+                    </ul>
+
+                    <p class="text-justify">Website ini merupakan project akhir yang saya kerjakan pada saat kelas 10. Project ini berbentuk website e-commerce yang dibuat menggunakan worpress.</p>
+                    <!-- /Project Description -->
+
+                    <!-- Technology -->
+                    <div class="tags-block">
+                        <div class="block-title">
+                            <h3>Technology</h3>
+                        </div>
+                        <ul class="tags">
+                            <li><a>WordPress</a></li>
+                        </ul>
+                    </div>
+                    <!-- /Technology -->
+
+                </div>
+                <!-- Project Description -->
+            </div>
+        </div>
+    </div>
+</div>
