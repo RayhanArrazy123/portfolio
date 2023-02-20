11b3dd3f1f1b753a58da4117355fed1f2f90f1bb
RayhanArrazy123 <125890079+RayhanArrazy123@users.noreply.github.com>
2023-02-20 22:27:48 +0700

    config

---
+++ config/footer.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+<footer class="site-footer clearfix">
+          <div class="footer-social">
+            <ul class="footer-social-links">
+              <li>
+                <a href="https://www.instagram.com/rayhan.projek/" target="_blank">My Instagram</a>
+              </li>
+            </ul>
+          </div>
+              
+          <div class="footer-copyrights">
+            <p>© 2022 All rights reserved. Rayhan Arrazy.</p>
+          </div>
+        </footer>
+        <!-- /Footer -->
---
+++ config/nav.php
@@ -0,0 +1,55 @@
+<!-- Text Logo -->
+<div class="text-logo">
+              <a href="index.php">
+                <div class="logo-symbol">R</div>
+                <div class="logo-text">Rayhan <span>Arrazy</span></div>
+              </a>
+            </div>
+            <!-- /Text Logo -->
+
+            <!-- Navigation -->
+            <div class="site-nav mobile-menu-hide">
+              <ul class="leven-classic-menu site-main-menu">
+                <li class="menu-item menu-item">
+                  <a href="index.php">About Me</a>
+                </li>
+
+                <li class="menu-item">
+                  <a href="resume.php">Resume</a>
+                </li>
+
+                <li class="menu-item menu-item">
+                  <a href="portfolio-2-columns.php">Portfolio</a>
+                  <!-- <ul class="sub-menu">
+                    <li class="menu-item">
+                      <a href="portfolio-2-columns.php">Portfolio 2 Columns</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="portfolio.php">Portfolio 3 Columns</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="portfolio-4-columns.php">Portfolio 4 Columns</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="portfolio-5-columns.php">Portfolio 5 Columns</a>
+                    </li>
+                  </ul> -->
+                </li>
+                
+                <!-- <li class="menu-item menu-item">
+                  <a href="blog.php">Blog</a>
+                  <ul class="sub-menu">
+                    <li class="menu-item">
+                      <a href="blog.php">Blog 2 Columns</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="blog-3-columns.php">Blog 3 Columns</a>
+                    </li>
+                  </ul>
+                </li> -->
+
+                <li class="menu-item">
+                  <a href="contact.php">Contact</a>
+                </li>
+              </ul>
+            </div>
