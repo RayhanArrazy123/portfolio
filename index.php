64420d65322ae26d035c61dbabb8a2aacdc40e8d
RayhanArrazy123 <125890079+RayhanArrazy123@users.noreply.github.com>
2023-02-20 22:39:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,218 @@
+<?php $page = "aboutpg"?>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+  
+<!-- Mirrored from lmpixels.com/demo/leven-html-new/full-width-dark/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 29 Dec 2022 09:08:39 GMT -->
+<head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <title>CV Rayhan Arrazy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="Leven - Resume / CV / vCard Template" />
+    <meta name="keywords" content="vcard, resposnive, retina, resume, jquery, css3, bootstrap, portfolio" />
+    <meta name="author" content="lmpixels" />
+    <!-- <link rel="shortcut icon" href="favicon.ico"> nanti bikin icon-->
+    <link rel="stylesheet" href="css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+
+    <script src="js/modernizr.custom.js"></script>
+  </head>
+
+  <body class="page">
+
+    <div class="lm-animated-bg"></div>
+    
+    <!-- Loading animation -->
+    <div class="preloader">
+      <div class="preloader-animation">
+        <div class="preloader-spinner">
+        </div>
+      </div>
+    </div>
+    <!-- /Loading animation -->
+
+    <!-- Scroll To Top Button -->
+    <div class="lmpixels-scroll-to-top"><i class="lnr lnr-chevron-up"></i></div>
+    <!-- /Scroll To Top Button -->
+
+    <div class="page-scroll">
+      <div id="page_container" class="page-container bg-move-effect" data-animation="transition-flip-in-right">
+
+        <!-- Header -->
+        <header id="site_header" class="header">
+          <div class="header-content clearfix">
+                
+            <?php include 'config/nav.php'; ?>
+
+            <!-- Mobile Menu Toggle -->
+            <a class="menu-toggle mobile-visible">
+              <i class="fa fa-bars"></i>
+            </a>
+            <!-- Mobile Menu Toggle -->
+          </div>
+        </header>
+        <!-- /Header -->
+
+        <div id="main" class="site-main">
+          <div id="main-content" class="single-page-content">
+            <div id="primary" class="content-area">    
+              <div id="content" class="page-content site-content single-post" role="main">
+                <!-- Home Page Top Part -->
+                <div class="row">
+                  <div class=" col-xs-12 col-sm-12">
+                    <div class="home-content">
+                      <div class="row flex-v-align">
+                        
+                        <div class="col-sm-12 col-md-5 col-lg-5">
+                          <div class="home-photo">
+                            <div class="hp-inner" style="background-image: url(img/pp.png);">
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="col-sm-12 col-md-7 col-lg-7">
+                          <div class="home-text hp-left">
+                            <div class="owl-carousel text-rotation">                                    
+                              <div class="item">
+                                <h4>Software Engineer</h4>
+                              </div>
+
+                              <div class="item">
+                                <h4>Web and Mobile App Designer</h4>
+                              </div>
+                            </div>
+                              
+                            <h1>Rayhan Arrazy</h1>
+                            <p>Saya adalah siswa SMK Budi Luhur. Saya memiliki kemampuan html dasar, css dasar dan php dasar. Saya sudah terbiasa menyelesaikan tugas sesuai dengan deadline. Saya juga terbiasa bekerja dalam tekanan.</p>
+                                 
+                            <div class="home-buttons">
+                              <a href="cv_pdf/CV Rayhan Arrazy.pdf" target="_blank" class="btn btn-primary">Download CV</a>
+                              <a href="contact.php" target="_self" class="btn btn-secondary">Contact</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- Home Page Top Part -->
+
+                <!-- Fun Facts -->
+                <div class="row">
+                  <div class="col-xs-12 col-sm-12">
+                    <div class="block-title">
+                      <h2>Skill</h2>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-xs-12 col-sm-6">
+                    <div class="lm-info-block gray-default">
+                      <!-- <i class="linecons linecons-heart"></i> -->
+                      <h4>Advance Beginner</h4>
+                      <span class="lm-info-block-value">HTML</span>
+                      <span class="lm-info-block-text"></span>
+                    </div>
+                  </div>
+
+                  <div class="col-xs-12 col-sm-6">
+                    <div class="lm-info-block gray-default">
+                      <!-- <i class="linecons linecons-clock"></i> -->
+                      <h4>Advance Beginner</h4>
+                      <span class="lm-info-block-value">CSS</span>
+                      <span class="lm-info-block-text"></span>
+                    </div>
+                  </div>
+
+                  <div class="col-xs-12 col-sm-6 ">
+                    <div class="lm-info-block gray-default">
+                      <!-- <i class="linecons linecons-star"></i> -->
+                      <h4>Advance Beginner</h4>
+                      <span class="lm-info-block-value">PHP Mysqli</span>
+                      <span class="lm-info-block-text"></span>
+                    </div>
+                  </div>
+
+                  <div class="col-xs-12 col-sm-6 ">
+                    <div class="lm-info-block gray-default">
+                      <!-- <i class="linecons linecons-cup"></i> -->
+                      <h4>Advance Beginner</h4>
+                      <span class="lm-info-block-value">WordPress</span>
+                      <span class="lm-info-block-text"></span>
+                    </div>
+                  </div>
+
+                  <div class="col-xs-12 col-sm-6 ">
+                    <div class="lm-info-block gray-default">
+                      <!-- <i class="linecons linecons-cup"></i> -->
+                      <h4>Novice Programmer</h4>
+                      <span class="lm-info-block-value">Android Studio</span>
+                      <span class="lm-info-block-text"></span>
+                    </div>
+                  </div>
+
+                  <div class="col-xs-12 col-sm-6 ">
+                    <div class="lm-info-block gray-default">
+                      <!-- <i class="linecons linecons-cup"></i> -->
+                      <h4>Novice</h4>
+                      <span class="lm-info-block-value">Adobe XD, Figma</span>
+                      <span class="lm-info-block-text"></span>
+                    </div>
+                  </div>
+                </div>
+                <!-- /Fun Facts -->
+
+                <div class="p-40"></div>
+
+                <!-- Pricing -->
+                <div class="row">
+                  
+
+                </div>
+
+                <div class="row">
+                  <div class=" col-xs-12 col-sm-12 ">
+                    <div class="fw-pricing clearfix row">
+                      
+
+                    
+
+                    
+
+                    
+                  </div>
+                </div> 
+                <!-- /Pricing -->
+              </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <?php include 'config/footer.php'; ?>
+        
+      </div>
+    </div>
+
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src='../../../../www.google.com/recaptcha/api.js'></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src='js/jquery.shuffle.min.js'></script>
+    <script src='js/masonry.pkgd.min.js'></script>
+    <script src='js/owl.carousel.min.js'></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrDf32aQTCVENBhFJbMBKOUTiUAABtC2o"></script>
+    <script src="js/jquery.googlemap.js"></script>
+    <script src="js/validator.js"></script>
+    <script src="js/main.js"></script>
+  <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"781164665dd187a5","version":"2022.11.3","r":1,"token":"94b99c0576dc45bf9d669fb5e9256829","si":100}' crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from lmpixels.com/demo/leven-html-new/full-width-dark/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 29 Dec 2022 09:08:50 GMT -->
+</html>
