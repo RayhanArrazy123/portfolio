3b8a0e88903815e29bea471249e35c5b1fa9aacb
RayhanArrazy123 <125890079+RayhanArrazy123@users.noreply.github.com>
2023-02-20 22:41:00 +0700

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,278 @@
+<?php $page = "resumepg"?>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+  
+<!-- Mirrored from lmpixels.com/demo/leven-html-new/full-width-dark/resume.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 29 Dec 2022 09:08:52 GMT -->
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <title>CV Rayhan Arrazy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="Leven - Resume / CV / vCard Template" />
+    <meta name="keywords" content="vcard, resposnive, retina, resume, jquery, css3, bootstrap, portfolio" />
+    <meta name="author" content="lmpixels" />
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+
+    <script src="js/modernizr.custom.js"></script>
+  </head>
+
+  <body class="page">
+
+    <div class="lm-animated-bg"></div>
+    
+    <!-- Loading animation -->
+    <div class="preloader">
+      <div class="preloader-animation">
+        <div class="preloader-spinner">
+        </div>
+      </div>
+    </div>
+    <!-- /Loading animation -->
+
+    <!-- Scroll To Top Button -->
+    <div class="lmpixels-scroll-to-top"><i class="lnr lnr-chevron-up"></i></div>
+    <!-- /Scroll To Top Button -->
+
+    <div class="page-scroll">
+      <div id="page_container" class="page-container bg-move-effect" data-animation="transition-flip-in-right">
+
+        <!-- Header -->
+        <header id="site_header" class="header">
+          <div class="header-content clearfix">
+                
+            <?php include 'config/nav.php'; ?>
+
+            <a class="menu-toggle mobile-visible">
+                <i class="fa fa-bars"></i>
+            </a>
+          </div>
+        </header>
+        <!-- /Header -->
+
+        <div id="main" class="site-main">
+          <div id="main-content" class="single-page-content">
+            <div id="primary" class="content-area">
+
+              <div class="page-title">
+                <h1>Resume</h1>
+                <div class="page-subtitle">
+                    <h4> Organization, Competition, and Work Experience</h4>
+                </div>
+              </div>
+
+              <div id="content" class="page-content site-content single-post" role="main">
+                <div class="row">
+  
+                  <div class=" col-xs-12 col-sm-6 ">
+                    <div class="block-title">
+                      <h2>Education and Competition</h2>
+                    </div>
+
+                    <div id="timeline_1" class="timeline clearfix">
+                      <div class="timeline-item clearfix">
+                        <h5 class="item-period ">Juli 2021 - Sekarang</h5>
+                        <span class="item-company">School</span>
+                        <h4 class="item-title">SMK Budi Luhur</h4>
+                        <p>Siswa SMK Budi Luhur jurusan Rekayasa Perangkat Lunak.</p>
+                      </div>
+
+                      <div class="timeline-item clearfix">
+                        <h5 class="item-period ">14 November 2021 dan 13 November 2022</h5>
+                        <span class="item-company">Competition</span>
+                        <h4 class="item-title">Mage 7 dan 8 ITS Surabaya</h4>
+                         <p>Peserta lomba App Development Mage 7 dan 8 yang diadakan oleh Institut Teknologi Sepuluh November.</p> 
+                      </div>
+                      
+                      <div class="timeline-item clearfix">
+                        <h5 class="item-period ">15 Februari 2022</h5>
+                        <span class="item-company">Competition</span>
+                        <h4 class="item-title">Seleksi Duta Anak Kota Tangerang</h4>
+                         <p>Menjadi 52 Semi Finalis Duta Anak Kota Tangerang Tahun 2022.</p> 
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class=" col-xs-12 col-sm-6 ">
+                    <div class="block-title">
+                      <h2>Experience</h2>
+                    </div>
+
+                    <div id="timeline_2" class="timeline clearfix">
+                      <div class="timeline-item clearfix">
+                        <h5 class="item-period current">Periode 2021/2022</h5>
+                        <span class="item-company">Organization</span>
+                        <h4 class="item-title">OSIS SMA dan SMK Budi Luhur</h4>
+                         <p>Wakil Ketua OSIS SMA dan SMK Budi Luhur periode 2021/2022.</p> 
+                      </div>
+
+                      <div class="timeline-item clearfix">
+                        <h5 class="item-period ">September - November 2021</h5>
+                        <span class="item-company">Work Experience</span>
+                        <h4 class="item-title">PT. ISKOM SARANA NUSANTARA</h4>
+                         <p>Setting windows, aplikasi ANBK, dan merimping kabel LAN dengan konektor RJ 45 untuk keperluan ANBK SMP Latahza Cikupa.</p> 
+                      </div>
+
+                      <div class="timeline-item clearfix">
+                        <h5 class="item-period ">7 Juni - Sekarang</h5>
+                        <span class="item-company">Work Experience</span>
+                        <h4 class="item-title">Marketing dan Komunikasi Website SMK</h4>
+                         <p>Saya ditugaskan menjadi admin blog untuk website smk.sekolahbudiluhur.com.</p> 
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class=" col-xs-12 col-sm-12 ">
+                    <div class="p-20"></div>
+
+                    <div class="block-title">
+                      <h2>Certificates</h2>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class=" col-xs-12 col-sm-6 ">
+                    <a href="img/clients/mage7.jpg" class="lightbox">
+                      <div class="certificate-item clearfix">
+                        <div class="certi-logo">
+                          <img src="img/clients/mage7logo.jpg" alt="logo">
+                        </div>
+
+                        <div class="certi-content">
+                          <div class="certi-title">
+                            <h4>Application Competition Mage 7</h4>
+                          </div>
+
+                          <div class="certi-id">
+                            <span>14 November 2021</span>
+                          </div>
+
+
+                          <div class="certi-company">
+                            <span></span>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div class=" col-xs-12 col-sm-6 ">
+                    <a href="img/clients/mage8.jpg" class="lightbox">
+                      <div class="certificate-item clearfix">
+                        <div class="certi-logo">
+                          <img src="img/clients/mage8logo.jpg" alt="logo">
+                        </div>
+
+                        <div class="certi-content">
+                          <div class="certi-title">
+                            <h4>Application Competition Mage 8</h4>
+                          </div>
+
+                          <div class="certi-id">
+                            <span>13 November 2022</span>
+                          </div>
+
+
+                          <div class="certi-company">
+                            <span></span>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div class=" col-xs-12 col-sm-6 ">
+                    <a href="img/clients/dutaanak.jpg" class="lightbox">
+                      <div class="certificate-item clearfix">
+                        <div class="certi-logo">
+                          <img src="img/clients/duta.png" alt="logo">
+                        </div>
+
+                        <div class="certi-content">
+                          <div class="certi-title">
+                            <h4>52 Semi Finalis Duta Anak Kota Tangerang</h4>
+                          </div>
+
+                          <div class="certi-id">
+                            <span>13 Maret 2022
+                            </span>
+                          </div>
+
+
+                          <div class="certi-company">
+                            <span></span>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div class=" col-xs-12 col-sm-6 ">
+                    <a href="img/clients/sertiosis.jpg" class="lightbox">
+                      <div class="certificate-item clearfix">
+                        <div class="certi-logo">
+                          <img src="img/clients/logoosis.jpg" alt="logo">
+                        </div>
+
+                        <div class="certi-content">
+                          <div class="certi-title">
+                            <h4>Wakil Ketua OSIS SMA dan SMK Budi Luhur</h4>
+                          </div>
+
+                          <div class="certi-id">
+                            <span>Periode 2021/2022</span>
+                          </div>
+
+
+                          <div class="certi-company">
+                            <span></span>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class=" col-xs-12 col-sm-12 ">
+                    <div class="p-40"></div>
+                  </div>
+                </div>
+
+                
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <?php include 'config/footer.php'; ?>
+        
+      </div>
+    </div>
+
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src='../../../../www.google.com/recaptcha/api.js'></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src='js/jquery.shuffle.min.js'></script>
+    <script src='js/masonry.pkgd.min.js'></script>
+    <script src='js/owl.carousel.min.js'></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrDf32aQTCVENBhFJbMBKOUTiUAABtC2o"></script>
+    <script src="js/jquery.googlemap.js"></script>
+    <script src="js/validator.js"></script>
+    <script src="js/main.js"></script>
+  <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"78116478dd9e898c","version":"2022.11.3","r":1,"token":"94b99c0576dc45bf9d669fb5e9256829","si":100}' crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from lmpixels.com/demo/leven-html-new/full-width-dark/resume.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 29 Dec 2022 09:08:55 GMT -->
+</html>
